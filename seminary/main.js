a5cee7883a2f82c023d40348b66a215bad09c262
David Kinnear <66265062+davidkinnear@users.noreply.github.com>
2020-10-15 00:53:55 -0500

    Add files via upload

---
+++ seminary/main.js
@@ -0,0 +1,6 @@
+function closeNav() {
+    document.getElementById("side_nav").style.display = "none";
+  }
+  function openNav() {
+    document.getElementById("side_nav").style.display = "flex";
+  }
